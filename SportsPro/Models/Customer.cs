eff958f06ca1e34a76be71f4a14a337cde12881c
basel abdul-aziz <basel.abdulazizz@gmail.com>
2020-10-06 11:02:12 -0600

    latest change basel

--- SportsPro/Models/Customer.cs
+++ SportsPro/Models/Customer.cs
@@ -7,27 +7,37 @@ namespace SportsPro.Models
     {
 		public int CustomerID { get; set; }
 
-		[Required]
+		
+		[Required(ErrorMessage = "Required")]
+		[StringLength(51, ErrorMessage = "Must be 51 characters or less")]
 		public string FirstName { get; set; }
 
-		[Required]
+		[Required(ErrorMessage = "Required")]
+		[StringLength(51, ErrorMessage = "Must be 51 characters or less")]
 		public string LastName { get; set; }
 
-		[Required]
+		[Required(ErrorMessage = "Required")]
+		[StringLength(51, ErrorMessage = "Must be 51 characters or less")]
 		public string Address { get; set; }
 
-		[Required]
+		[Required(ErrorMessage = "Required")]
+		[StringLength(51, ErrorMessage = "Must be 51 characters or less")]
 		public string City { get; set; }
 
-		[Required]
+		[Required(ErrorMessage = "Required")]
+		[StringLength(51, ErrorMessage = "Must be 51 characters or less")]
 		public string State { get; set; }
 
-		[Required]
+		[Required(ErrorMessage = "Required")]
+		[StringLength(21, ErrorMessage = "Must be 21 characters or less")]
 		public string PostalCode { get; set; }
 
 		[Required]
 		public string CountryID { get; set; }
 		public Country Country { get; set; }
+
+		[RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$",
+			ErrorMessage = "Entered phone format is not valid.")]
 
 		public string Phone { get; set; }
 
